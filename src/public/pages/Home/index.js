0ad3af07a72ec1a27cbc1d189c04e35d17da48ed
Andryel Blanco <andryelmblanco@gmail.com>
2021-08-09 20:00:04 -0300

    first commit

---
+++ src/public/pages/Home/index.js
@@ -0,0 +1,12 @@
+import './Styles.scss';
+
+import Logo from '../../assets/images/Logo.svg';
+
+export default function HomePage() {
+  return (
+    <div className="container-page">
+      <img src={Logo}/>
+    </div>
+  );
+}
+
